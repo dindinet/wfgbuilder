307d55d8d005d90ed8750227cfcc4e63128ad834
dindinet <dklongley@gmail.com>
2025-08-18 15:20:18 +0100

    fix postcss config

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+const autoprefixer = require('autoprefixer');
+const purgeCSSPlugin = require('@fullhuman/postcss-purgecss').default;
+
+const purgecss = purgeCSSPlugin({
+  content: ['./hugo_stats.json'],
+  defaultExtractor: content => {
+    const els = JSON.parse(content).htmlElements;
+    return [
+      ...(els.tags || []),
+      ...(els.classes || []),
+      ...(els.ids || []),
+    ];
+  },
+  // https://purgecss.com/safelisting.html
+  safelist: []
+});
+
+module.exports = {
+  plugins: [
+    process.env.HUGO_ENVIRONMENT !== 'development' ? purgecss : null,
+    autoprefixer,
+  ]
+};
