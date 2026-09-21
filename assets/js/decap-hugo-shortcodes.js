675fa06d5bff6e661b3c676295be48c9443e5878
dindinet <2939526+dindinet@users.noreply.github.com>
2026-09-21 15:50:15 +0100

    decap hugo shortcodes

---
+++ assets/js/decap-hugo-shortcodes.js
@@ -0,0 +1,71 @@
+CMS.registerEditorComponent({
+    id: "figure",
+    label: "Figure",
+    fields: [{
+            name: "title",
+            label: "Figure Title",
+            widget: "string"
+        },
+        {
+            name: "src",
+            label: "Figure SRC",
+            widget: "string"
+        },
+    ],
+    pattern: /{{< figure src="([a-zA-Z0-9-_ ]+)" title="([a-zA-Z0-9-_ ]+)" >}}/,
+    fromBlock: function(match) {
+        return {
+            title: match[1],
+            src: match[2],
+        };
+    },
+    toBlock: function(obj) {
+        return `{{< figure src="${obj.src}" title="${obj.title}" >}}`;
+    },
+    toPreview: function(obj) {
+        return `<figure><img src=${obj.src} alt=${obj.title}><figcaption>${obj.title}</figcaption></figure>`;
+    },
+});
+
+CMS.registerEditorComponent({
+    id: "vimeo",
+    label: "Vimeo",
+    fields: [{
+        name: "shortcode",
+        label: "Vimeo shortcode",
+        widget: "string"
+    }],
+    pattern: /{{< vimeo ([a-zA-Z0-9]+) >}}/,
+    fromBlock: function(match) {
+        return {
+            shortcode: match[1]
+        };
+    },
+    toBlock: function(obj) {
+        return `{{< vimeo ${obj.shortcode} >}}`;
+    },
+    toPreview: function(obj) {
+        return `{{< vimeo ${obj.shortcode} >}}`;
+    },
+});
+CMS.registerEditorComponent({
+    id: "youtube",
+    label: "Youtube",
+    fields: [{
+        name: "id",
+        label: "Youtube Video ID",
+        widget: "string"
+    }],
+    pattern: /{{< youtube\s+(?<id>[A-Za-z0-9\-]+)\s+>}}/,
+    fromBlock: function(match) {
+        return {
+            id: match[1],
+        };
+    },
+    toBlock: function(obj) {
+        return `{{< youtube ${obj.id} >}}`;
+    },
+    toPreview: function(obj) {
+        return `<img src="https://i3.ytimg.com/vi/${obj.id}/hqdefault.jpg" alt="Youtube Video"/>`;
+    },
+});
