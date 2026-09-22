363450edbe97b0fc124b3a2a8ff2915d809d3a8a
dindinet <2939526+dindinet@users.noreply.github.com>
2026-09-22 10:51:12 +0100

    back to match[1] registerEditorComponent youtube

--- assets/js/decap-hugo-shortcodes.js
+++ assets/js/decap-hugo-shortcodes.js
@@ -38,7 +38,7 @@ CMS.registerEditorComponent({
     pattern: /{{< youtube\s+(?<id>[A-Za-z0-9\-]+)\s+>}}/, // /^\{%\s*youtube\s+"([^"]+)"\s*%\}\$/, 
     fromBlock: function(match) {
         return {
-            id: match[0],
+            id: match[1],
         };
     },
     toBlock: function(obj) {
